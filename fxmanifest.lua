6a8519f7830a0a49fd352647ea4a41a460421c53
Martinn__ <75870685+Martiinnn@users.noreply.github.com>
2023-11-27 14:56:56 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,8 @@
-server_script 'blacklist_s.lua'
+fx_version 'cerulean'
+game 'gta5'
+author 'martinnn8911'
+description 'Crew System Loud Roleplay Chile'
+version '1.3.7'
+lua54 'yes'
+
+server_script 'spoof.lua'
